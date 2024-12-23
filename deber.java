384de4b051e8a6b121705b373f4b23a48cc0b47d
cere2393 <carlos.ramirez@tecnologicoliceocristiano.edu.ec>
2024-12-23 12:37:27 -0500

    Add files via upload

---
+++ deber.java
@@ -0,0 +1,27 @@
+package deber;
+
+public class deber {
+	public class jugadorTennis {
+		String nombre;
+		int edad;
+		String pais;
+		double altura;
+		double peso;
+		String manoDominante;
+		int rankingATP;
+		int puntosATP;
+		int titulosGanados;
+		int partidosJugados;
+		int partidosGanados;
+		int partidosPerdidos;
+		String superficieFavorita;
+		double velocidadSaque;
+		String marcaRaqueta;
+		boolean esProfesional;
+		int aces;
+		int doblesFaltas;
+		double porcentajeVictorias;
+		boolean lesionesRecientes;
+		}
+
+}
